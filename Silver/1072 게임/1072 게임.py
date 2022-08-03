0ce13129e10bf5fdf39af3a10094c20bfd7e5ad9
jooooonis <xowns3213@naver.com>
2022-08-03 17:41:27 +0900

    코드 수정

--- Silver/1072 게임/1072 게임.py
+++ Silver/1072 게임/1072 게임.py
@@ -3,18 +3,18 @@ input = sys.stdin.readline
 
 x, y = map(int, input().rsplit())
 z = y * 100 // x
-left, right = 1, x
+left, right = 1, x 
 answer = sys.maxsize
 
 
 while left <= right:
     mid = (left + right) // 2
     w = (y + mid) * 100 // (x + mid)
-    if w > z:
+    if w <= z:
+        left = mid + 1
+    else:
         answer = min(answer, mid)
         right = mid - 1
-    else:
-        left = mid + 1
 
 if z >= 99:
     print(-1)

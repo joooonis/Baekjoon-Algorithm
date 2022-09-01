3c84439a0fa2f4c8d4f78c0ea9dde6874b2e4715
jooooonis <xowns3213@naver.com>
2022-09-01 20:37:55 +0900

    solve : 2178

---
+++ Silver/2178 미로 탐색/2178 미로 탐색.py
@@ -0,0 +1,43 @@
+import sys
+sys.setrecursionlimit(10**9)
+
+n, m = map(int, sys.stdin.readline().split())
+
+graph,counts = [],[]
+
+for _ in range(n):
+    line = sys.stdin.readline().rstrip()
+    graph.append([i for i in line ])
+
+visited = [[ False for _ in range(m)] for _ in range(n)]
+
+from collections import deque
+
+
+dx = [-1, 1, 0 ,0]
+dy = [0, 0, -1, 1]
+
+graph[0][0] = 1
+queue = [[0,0]]
+    
+while queue:
+    x, y = queue[0][0], queue[0][1]
+    del queue[0]
+    
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx >= 0 and ny >= 0 and nx < n and ny < m:
+            if graph[nx][ny] == '1':
+                queue.append([nx,ny])
+                graph[nx][ny] = graph[x][y] + 1          
+                            
+print(graph)
+
+    
+
+
+
+
+
+            

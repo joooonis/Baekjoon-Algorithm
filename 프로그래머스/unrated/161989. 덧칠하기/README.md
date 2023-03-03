8010cd2886f75118d1bb3b437c6bd5b4fc1e6556
joooonis <92621861+joooonis@users.noreply.github.com>
2023-03-04 00:26:24 +0900

    [unrated] Title: 덧칠하기, Time: 175.14 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/README.md
+++ 프로그래머스/unrated/161989. 덧칠하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 덧칠하기 - 161989 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989#qna) 
 
 ### 성능 요약
 
-메모리: 39.1 MB, 시간: 86.81 ms
+메모리: 39.1 MB, 시간: 175.14 ms
 
 ### 구분
 
--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -8,16 +8,6 @@ function solution(n, m, section) {
     while (section.length > 0 && section[0] <= next + m - 1) {
       section.shift();
     }
-
-    if (section.length === 0) {
-      break;
-    }
-    next = section.pop();
-    answer++;
-
-    while (section.length > 0 && section[section.length - 1] >= next - m + 1) {
-      section.pop();
-    }
   }
 
   return answer;

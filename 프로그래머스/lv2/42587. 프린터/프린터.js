131382708f23085ec9e188ab2aedc89845c29650
joooonis <92621861+joooonis@users.noreply.github.com>
2023-03-25 00:58:30 +0900

    [level 2] Title: 프린터, Time: 4.26 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.js
@@ -0,0 +1,21 @@
+function solution(priorities, location) {
+    const queue = priorities.map((v,i)=>[v,i])
+    
+    console.log(queue)
+    
+    let count = 1
+
+    
+    while(queue.length > 0){
+
+        paper = queue.shift()
+        if (Math.max(...queue.map(v=>v[0])) > paper[0]){
+            queue.push(paper)
+        } else if (paper[1] === location ){
+            return count
+        } else {
+            count++
+        }
+    }
+    return count
+}

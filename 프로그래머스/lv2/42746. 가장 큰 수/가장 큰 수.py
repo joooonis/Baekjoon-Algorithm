3eadfcad1dce976eef0daabed58402bc1de7ec42
joooonis <92621861+joooonis@users.noreply.github.com>
2023-03-25 16:51:21 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,23 +1,4 @@
 def solution(numbers):
-    numbers = list(map(str, numbers))
-    quick_sort(numbers, 0, len(numbers)-1)
-    return str(int(''.join(numbers)))
-
-def quick_sort(numbers, start, end):
-    if start >= end: # 원소가 1개인 경우 종료
-        return
-    
-    pivot = partition(numbers, start, end) # pivot을 기준으로 분할
-    quick_sort(numbers, start, pivot-1)
-    quick_sort(numbers, pivot+1, end)
-
-def partition(numbers, start, end):
-    pivot = numbers[end] # 리스트의 마지막 값을 피벗으로 설정
-    i = start - 1
-    for j in range(start, end):
-        # 두 문자열을 이어 붙였을 때 큰 값을 앞으로 오도록 비교
-        if numbers[j] + pivot > pivot + numbers[j]:
-            i += 1
-            numbers[i], numbers[j] = numbers[j], numbers[i]
-    numbers[i+1], numbers[end] = numbers[end], numbers[i+1]
-    return i+1
+    numbers = list(map(str, numbers)) # 숫자들을 문자열로 변환
+    numbers.sort(key=lambda x: x*3, reverse=True) # 문자열을 이어 붙인 후 큰 순서대로 정렬
+    return str(int(''.join(numbers))) # 0이 여러 개인 경우를 대비하여 int로 변환한 후 다시 str로 변환

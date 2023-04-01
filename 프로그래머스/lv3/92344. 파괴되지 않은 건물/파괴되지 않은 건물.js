f4e808167b163ea2e9d47926fe5bb2ec1e9fa41f
joooonis <92621861+joooonis@users.noreply.github.com>
2023-04-01 16:18:05 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 100.78 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
+++ 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
@@ -1,42 +1,44 @@
 function effect(board, skill) {
-    let damage = skill[5];
-    if (skill[0] == 1) damage = damage*(-1);
-    board[skill[1]][skill[2]] += damage;
-    if (skill[4]+1 < board[0].length) board[skill[1]][skill[4]+1] -= damage;
-    if (skill[3]+1 < board.length) board[skill[3]+1][skill[2]] -= damage;
-    if (skill[4]+1 < board[0].length && skill[3]+1 < board.length) board[skill[3]+1][skill[4]+1] += damage;
+	const row = board.length;
+	const col = board[0].length;
+	const [type, r1, c1, r2, c2, degree] = skill;
+	let damage = degree;
+	if (type == 1) damage = damage * -1;
+	board[r1][c1] += damage;
+	if (c2 + 1 < col) board[r1][c2 + 1] -= damage;
+	if (r2 + 1 < row) board[r2 + 1][c1] -= damage;
+	if (c2 + 1 < col && r2 + 1 < row) board[r2 + 1][c2 + 1] += damage;
 }
 
 function solution(board, skill) {
-    var answer = 0;
-    let aBoard = new Array(board.length).fill(0).map(e => new Array(board[0].length).fill(0))
-    for(let i in skill) {
-        effect(aBoard,skill[i])
-        //console.log(aBoard)
-    }
+	var answer = 0;
+	let aBoard = Array(board.length)
+		.fill(0)
+		.map((_) => Array(board[0].length).fill(0));
 
-    for(let i=0; i<aBoard.length; i++) {
-        for (let j=1; j<aBoard.length; j++) {
-            aBoard[i][j] += aBoard[i][j-1]; 
-        }
-    }
+	for (let s of skill) {
+		effect(aBoard, s);
+	}
 
-    for(let i=0; i<aBoard.length; i++) {
-        for (let j=1; j<aBoard.length; j++) {
-            aBoard[j][i] += aBoard[j-1][i]; 
-        }
-    }
-
-    for(let i=0; i<aBoard.length; i++) {
-        for (let j=0; j<aBoard.length; j++) {
-            board[i][j] += aBoard[i][j]; 
-        }
-    }
-
-    for(let i in board) {
-        for (let j in board[i]) {
-            if(board[i][j] > 0) answer += 1; 
-        }
-    }
-    return answer;
+	for (let i = 0; i < aBoard.length; i++) {
+		for (let j = 1; j < aBoard.length; j++) {
+			aBoard[i][j] += aBoard[i][j - 1];
+		}
+	}
+	for (let i = 0; i < aBoard.length; i++) {
+		for (let j = 1; j < aBoard.length; j++) {
+			aBoard[j][i] += aBoard[j - 1][i];
+		}
+	}
+	for (let i = 0; i < aBoard.length; i++) {
+		for (let j = 0; j < aBoard.length; j++) {
+			board[i][j] += aBoard[i][j];
+		}
+	}
+	for (let i in board) {
+		for (let j in board[i]) {
+			if (board[i][j] > 0) answer += 1;
+		}
+	}
+	return answer;
 }

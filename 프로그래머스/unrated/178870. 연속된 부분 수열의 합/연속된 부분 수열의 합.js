6481ffaf9cf36a7b5676600fdabe2538f2d8ae2f
joooonis <92621861+joooonis@users.noreply.github.com>
2023-04-08 14:24:01 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,19 +1,20 @@
 function solution(sequence, k) {
-  const map = new Map([[0, 0]]);
-  let sum = 0;
-  let answers = [];
-  for (let i = 0; i < sequence.length; i++) {
-    sum += sequence[i];
-    if (map.has(sum - k)) {
-      answers.push([map.get(sum - k), i]);
+    let answers = [];
+    
+    const map = new Map([[0,0]])
+    let sum = 0
+    
+    for (let i=0; i<sequence.length; i++){
+        sum += sequence[i]
+        if (map.has(sum - k)){
+            answers.push([map.get(sum-k), i])
+        }
+        map.set(sum, i + 1)
     }
-    map.set(sum, i + 1);
-  }
-
-  answers.sort((a, b) => {
-    if (a[1] - a[0] > b[1] - b[0]) return 1;
-    else if (a[1] - a[0] === b[1] - b[0]) return a[0] - b[0];
-    else return -1;
+    
+     answers.sort((a, b) => {
+    if (a[1] - a[0] !== b[1] - b[0]) return a[1] - a[0] - (b[1] - b[0]);
+    else return a[0] - b[0];
   });
-  return answers[0];
+    return answers[0];
 }

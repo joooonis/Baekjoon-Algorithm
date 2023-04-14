a14bc0ad5ca12632b777498f46bf75a0f243e2ff
joooonis <92621861+joooonis@users.noreply.github.com>
2023-04-14 17:26:04 +0900

    [level 3] Title: 단어 변환, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -1,37 +1,44 @@
 function solution(begin, target, words) {
-  let answer = Infinity;
-  const visited = Array(words.length).fill(false);
+    var answer = 0;
+    words.push(begin)
+    const graph = Array(words.length).fill().map(()=>[])
+    
 
-  const dfs = (current, count) => {
-    if (current === target) {
-      answer = Math.min(count, answer);
-      return;
+    for (let i=0; i<words.length-1; i++){
+        for (let j=i+1; j<words.length; j++){
+            if (isConnected(words[i],words[j])){
+                graph[i].push(j)
+                graph[j].push(i)
+            }
+        }
+    }
+    
+    const visited = Array(words.length).fill(false)
+
+    let answers = []
+    
+    function dfs(node, target, count){
+        visited[node] = true
+        if (node === target) answers.push(count)
+        for (let next of graph[node]){
+            if (!visited[next]) {
+                dfs(next,target,count+1)
+                visited[next] = false
+            }
+        }
+        
     }
 
-    for (let i = 0; i < words.length; i++) {
-      if (isConnect(current, words[i]) && !visited[i]) {
-        visited[i] = true;
-        dfs(words[i], count + 1);
-        visited[i] = false;
-      }
-    }
-  };
-
-  if (!words.includes(target)) {
-    return 0;
-  }
-
-  dfs(begin, 0);
-
-  return answer !== Infinity ? answer : 0;
+    let start = words.indexOf(begin)
+    let end = words.indexOf(target)
+    dfs(start, end, 0)
+    return answers.length ? Math.min(...answers) : 0;
 }
 
-const isConnect = (a, b) => {
-  let count = 0;
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
-      count++;
+const isConnected = (w1, w2) => {
+    let diff = 0
+    for (let i=0; i<w1.length; i++){
+        if (w1[i] !== w2[i]) diff++
     }
-  }
-  return count === 1;
-};
+    return diff === 1
+}

37bf4e734b3ef1b0beed471a2e3dc5c071b6c58d
joooonis <92621861+joooonis@users.noreply.github.com>
2023-04-14 14:25:20 +0900

    [level 3] Title: 베스트앨범, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -1,24 +1,22 @@
 function solution(genres, plays) {
-  let hash = {};
-  let playsSum = {};
-  let answer = [];
+    genreHash = {}
+    playHash = {}
+    genres.forEach((genre,idx) => {
+        if (!genreHash[genre]) genreHash[genre] = [idx] 
+        else genreHash[genre].push(idx)
+        
+        playHash[genre] = (playHash[genre] + plays[idx]) ||  plays[idx]
+    })
+    let answer = [];
 
-  for (let i = 0; i < genres.length; i++) {
-    hash[genres[i]] ||= [];
-    hash[genres[i]].push([plays[i], i]);
-    playsSum[genres[i]] = (playsSum[genres[i]] || 0) + plays[i];
-  }
-
-  const orders = Object.entries(playsSum).sort((a, b) => b[1] - a[1]);
-
-  for (let [genre, _] of orders) {
-    hash[genre].sort((a, b) => b[0] - a[0]);
-    if (hash[genre].length === 1) {
-      answer.push(hash[genre][0][1]);
-    } else {
-      answer.push(hash[genre][0][1]);
-      answer.push(hash[genre][1][1]);
+    keys = Object.keys(genreHash)
+    keys.sort((a,b) => playHash[b] - playHash[a])
+    
+    for (let key of keys){
+        genreHash[key].sort((a,b) => plays[b] - plays[a] || a - b)
+        answer = answer.concat(genreHash[key].slice(0,2))
     }
-  }
-  return answer;
+    
+    
+    return answer;
 }

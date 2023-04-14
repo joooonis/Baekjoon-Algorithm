f0c3c1e2031304f53a2f248ea55d69861f5a8f17
joooonis <92621861+joooonis@users.noreply.github.com>
2023-04-14 16:52:55 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.js
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -1,20 +1,19 @@
 function solution(n, computers) {
-  const visited = new Array(n).fill(false);
-  let clusters = 0;
+  let network = 0;
+  const visited = Array(n).fill(false);
+
   function dfs(node) {
     visited[node] = true;
     for (let neighbor = 0; neighbor < n; neighbor++) {
-      if (computers[node][neighbor] === 1 && !visited[neighbor]) {
-        dfs(neighbor);
-      }
+      if (computers[node][neighbor] === 1 && !visited[neighbor]) dfs(neighbor);
     }
   }
 
   for (let node = 0; node < n; node++) {
     if (!visited[node]) {
       dfs(node);
-      clusters++;
+      network++;
     }
   }
-  return clusters;
+  return network;
 }

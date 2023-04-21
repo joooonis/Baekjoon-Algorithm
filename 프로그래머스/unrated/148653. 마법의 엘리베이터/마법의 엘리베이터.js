edb7f0dafce418872e4a1fd25d37d72c5dcbf9b2
joooonis <92621861+joooonis@users.noreply.github.com>
2023-04-21 14:05:01 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -2,12 +2,14 @@ function solution(storey) {
   let answer = 0;
   while (storey > 0) {
     const p = storey % 10;
-    if (p >= 6){
-              answer += 10 - p;
+    if (p > 5) {
+      answer += 10 - p;
       storey += 10;
-
-    }
-    else if (p == 5 && (storey+'').length >= 2 && (storey + '')[(storey+'').length - 2] >= 5) {
+    } else if (
+      p == 5 &&
+      (storey + '').length >= 2 &&
+      (storey + '')[(storey + '').length - 2] >= 5
+    ) {
       answer += 10 - p;
       storey += 10;
     } else if (p > 0) {

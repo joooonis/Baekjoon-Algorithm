8c18d15365e7a945943c82678468fb16cdbd2cec
joooonis <92621861+joooonis@users.noreply.github.com>
2023-04-24 14:46:11 +0900

    [level 2] Title: 할인 행사, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/README.md
+++ 프로그래머스/lv2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.13 ms
+메모리: 33.5 MB, 시간: 0.11 ms
 
 ### 구분
 
--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
@@ -1,20 +1,19 @@
 function solution(want, number, discount) {
   answer = 0;
+  hash = {};
+  for (let i = 0; i < 10; i++) {
+    hash[discount[i]] = (hash[discount[i]] || 0) + 1;
+  }
 
-  const isMatch = (discount) => {
-    hash = {};
-    for (let d of discount) {
-      hash[d] = (hash[d] || 0) + 1;
-    }
-    let enough = true;
-    want.forEach((w, i) => {
-      if (hash[w] !== number[i]) enough = false;
-    });
-    return enough;
-  };
+  if (want.filter((w, i) => hash[w] === number[i]).length === want.length)
+    answer++;
 
-  for (let i = 0; i <= discount.length - 10; i++) {
-    if (isMatch(discount.slice(i, i + 10))) answer += 1;
+  for (let i = 10; i < discount.length; i++) {
+    hash[discount[i - 10]] -= 1;
+    hash[discount[i]] = (hash[discount[i]] || 0) + 1;
+
+    if (want.filter((v, i) => hash[v] === number[i]).length === want.length)
+      answer++;
   }
 
   return answer;
